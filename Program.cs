91a4c119a2f059ad9702a0ef5fcbf764f780963c
MansiRavindraMohan <manasimohan29@gmail.com>
2023-02-08 07:47:58 +0530

    First commit file

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //flipCoin.CalculatePercentage();
+            //TableofTwo.PowerofTwo(); 
+            Harmonic.numberharmonic();
+            
+            //int num = 7;
+            //double num1 = 10.6;
+            //bool val = true;
+            //char singleCharacter = 'm';
+            //string name = "Mansi";
+            //Console.WriteLine("This is an integer value " + num);
+            //Console.WriteLine("This is an double value " + num1);
+            //Console.WriteLine("This is an bool value " + val);
+            //Console.WriteLine("This is an character value " + singleCharacter);
+            //Console.WriteLine("This is an string value " + name);
+            //Console.WriteLine("Hi, Hello, How are you?");
+
+
+            //string nameofPerson = Console.ReadLine();
+            //Console.WriteLine("This is name " + nameofPerson);
+            //Console.WriteLine("Print the username ");
+            //string username = Console.ReadLine();
+            //if (username.Length > 3)
+            //{
+            //    Console.WriteLine("Hello " + username + " How are you?");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The name is not having minimum 3 characters");
+            //}
+
+            //Console.WriteLine("Enter the first number");
+            //int num1=Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Enter the second number");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+
+            //int num3=num1+num2;
+            //Console.WriteLine("The addition of two numbers " + num3);
+
+            //int chk_year;
+            //Console.Write("Input an year : ");
+            //chk_year = Convert.ToInt32(Console.ReadLine());
+
+            //if ((chk_year % 400) == 0)
+            //    Console.WriteLine("{0} is a leap year.\n", chk_year);
+            //else if ((chk_year % 100) == 0)
+            //    Console.WriteLine("{0} is not a leap year.\n", chk_year);
+            //else if ((chk_year % 4) == 0)
+            //    Console.WriteLine("{0} is a leap year.\n", chk_year);
+            //else
+            //    Console.WriteLine("{0} is not a leap year.\n", chk_year);
+        }
+    }
+}
