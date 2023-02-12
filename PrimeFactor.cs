359f0f52b5f1c781bb9419d2710a19d9566f3433
MansiRavindraMohan <manasimohan29@gmail.com>
2023-02-12 14:53:36 +0530

    Displaying prime factors of a number

---
+++ PrimeFactor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class PrimeFactor
+    {
+        public static void FactorPrime()
+        {
+            int n, i;
+            Console.WriteLine("Enter the Number:");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Factors:");
+            for (i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
